81f276a51c5912e3ff97d18716724320919ba398
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-04-24 13:29:14 -0300

    .
    .

---
+++ Tipos_dados.rb
@@ -0,0 +1,19 @@
+# Tipagem dinâmica
+
+produto = 'Macbook'
+preco = 5999.99
+quantidade = 10
+disponivel = true
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel
+
+quantidade = '9'
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
+
