d1458cfd6ea18b9d8915d282f3cb6ea10b2b176e
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-05-30 08:00:15 -0300

    senior - 20190530 - m
    senior - 20190530 - m

--- unitarios/spec/avengers_spec.rb
+++ unitarios/spec/avengers_spec.rb
@@ -16,9 +16,48 @@ describe AvengersHeadQuarter do
 
     it 'Avenger add' do
         hq = AvengersHeadQuarter.new
-        
         hq.put('Spiderman')
-        expect(hq.list).to eql ['Spiderman']
+        expect(hq.list).to include 'Spiderman'
     end
 
+    it 'Do add Avenger list' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        res = hq.list.size > 0
+        expect(res).to be true
+        expect(res).to be false #Forçar erro
+        expect(hq.list.size).to be > 0
+
+        expect(hq.list).to include 'Thor'
+        expect(hq.list).to include 'IronMan'
+    end
+
+    it 'thor must to be 1st in list' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        expect(hq.list).to start_with('Thor')
+    end
+
+    it 'ironman must to be ast in list' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+        hq.put('IronMan')
+
+        expect(hq.list).to end_with('IronMan')
+    end
+
+    it 'must contain last name' do
+        avenger = 'Peter Parker'
+
+        expect(avenger).to match(/Parker/)
+        expect(avenger).not_to match(/Pak/)
+    end
 end 
